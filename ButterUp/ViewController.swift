c34fbbd40f36ee59b4a3c3ba5c9674396a11eefa
simon <schmalfuss.simon@gmail.com>
2015-10-29 11:28:36 -0700

    Initial Commit

---
+++ ButterUp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ButterUp
+//
+//  Created by Horst Schmalfuß on 29.10.15.
+//  Copyright (c) 2015 ButterUp inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ButterUpTests/ButterUpTests.swift
@@ -0,0 +1,36 @@
+//
+//  ButterUpTests.swift
+//  ButterUpTests
+//
+//  Created by Horst Schmalfuß on 29.10.15.
+//  Copyright (c) 2015 ButterUp inc. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class ButterUpTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
